c6a7a72da63aa438a14a859e03418049c1ffd80a
massenandev <massenandev@gmail.com>
2020-05-15 22:53:29 -0300

    updating records on db

--- src/jdbc/ConsultarPessoas2.java
+++ src/jdbc/ConsultarPessoas2.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 
@@ -24,18 +22,7 @@ public class ConsultarPessoas2 {
 		PreparedStatement stmt = conexao.prepareStatement(sql);
 		stmt.setString(1, "%" + valor + "%");
 		ResultSet resultado = stmt.executeQuery();
-		
-		List<Pessoa> pessoas = new ArrayList<>();
-		
-		while(resultado.next()) {
-			int codigo = resultado.getInt("codigo");
-			String nome = resultado.getString("nome");
-			pessoas.add(new Pessoa(codigo,nome));
-		}
-		
-		for(Pessoa p : pessoas) {
-			System.out.println(p.getCodigo() + " ==> " + p.getNome());
-		}
+	
 		
 		sc.close();
 		stmt.close();
